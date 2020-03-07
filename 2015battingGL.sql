d94944cbcf0f2fc3a2e840df64983ffa4c8a0fc6
geleigh <57293957+geleigh@users.noreply.github.com>
2020-03-07 12:33:48 -0500

    final push v.2

--- 2015battingGL.sql
+++ 2015battingGL.sql
@@ -19,14 +19,3 @@ CREATE TABLE team_id (
 Team TEXT PRIMARY KEY,
 Tm TEXT,
 );
-
-CREATE TABLE attendance (
-Tm TEXT PRIMARY KEY,
-Attendance INT;)
-
--- JOINS TABLES 
-
-SELECT fifteen_defense.TEAM, customer_name.first_name, customer_name.last_name, customer_location.address, customer_location.us_state
-FROM fifteen_batting
-JOIN team_id
-ON fifteen_batting.TEAM = attendance.;
