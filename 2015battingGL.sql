e3fda1186528e8d65c30a54da9726274f4423c74
geleigh <57293957+geleigh@users.noreply.github.com>
2020-03-07 11:37:08 -0500

    updated sql and db connection

--- 2015battingGL.sql
+++ 2015battingGL.sql
@@ -1,4 +1,4 @@
-## MLB SQL BATTING REFERENCE
+-- MLB SQL BATTING REFERENCE
 
 CREATE TABLE fifteen_batting (
 TEAM TEXT PRIMARY KEY,
@@ -15,3 +15,16 @@ RUNS INT,
 HOMERUNS INT,
 );
 
+CREATE TABLE team_id (
+TEAM TEXT PRIMARY KEY,
+)
+
+CREATE TABLE attendance (
+TEAM TEXT PRIMARY KEY)
+
+-- JOINS TABLES 
+
+SELECT fifteen_defense.TEAM, customer_name.first_name, customer_name.last_name, customer_location.address, customer_location.us_state
+FROM fifteen_batting
+JOIN team_id
+ON fifteen_batting.TEAM = attendance.;
